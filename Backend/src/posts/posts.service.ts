cb18e2db8d953fecb26868272205d5168235601a
ValeriiSharaev <125194505+ValeriiSharaev@users.noreply.github.com>
2024-07-24 10:00:58 +0400

    Update posts.service.ts

--- Backend/src/posts/posts.service.ts
+++ Backend/src/posts/posts.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import {PrismaService} from "../prisma/prisma.service";
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+    constructor(private prisma: PrismaService) {
+    }
+
+
+}
